46d9707c60eedb7c6e9520a065eb10ee0dc43c9f
Prudhvi Raj Tappeta <prudhvitappeta@gmail.com>
2023-03-25 22:37:33 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,34 @@
+const heading = React.createElement("h1", {id:"heading", xyz:"tagValue"}, "Hello World from React!");
+console.log(heading); // retunrs JS object which is React Element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+/** 
+ * Takes heading object which is React Elemet
+ * Converts that React Element into HTML tag which can be understand by Browser
+ * Reander this HTML tag inside the root element.(appendChild)
+*/
+//root.render(heading); 
+
+/**
+ * <div id="parent">
+*    <div id="child1">
+ *      <h1>I am h1 Tag </h1>
+ *      <h2>I am h2 Tag </h2>
+ *    </div>
+ *    <div id="child2">
+ *      <h1>I am h1 Tag </h1>
+ *      <h2>I am h2 Tag </h2>
+ *    </div>
+ * </div>
+ */
+
+const parent =  React.createElement("div", {id:"parent"}, [
+    React.createElement("div", {id:"child1"},[ 
+        React.createElement("h1", {}, "I am H1 Tag"),
+        React.createElement("h2", {}, "I am H2 Tag")] 
+    ),
+    React.createElement("div", {id:"child2"}, [
+        React.createElement("h1", {}, "I am H1 Tag"),
+        React.createElement("h2", {}, "I am H2 Tag")] 
+    )
+]);
+root.render(parent); 
