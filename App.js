00fdd634aa17ae2cb3941d78cc20afa5a1aed181
Prudhvi Raj Tappeta <prudhvitappeta@gmail.com>
2023-03-26 01:13:06 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", {id:"heading", xyz:"tagValue"}, "Hello World from React!");
 console.log(heading); // retunrs JS object which is React Element
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent =  React.createElement("div", {id:"parent"}, [
+    React.createElement("div", {id:"child1"},[ 
+        React.createElement("h1", {}, "I am H1 Tag"),
+        React.createElement("h2", {}, "I am H2 Tag")] 
+    ),
+    React.createElement("div", {id:"child2"}, [
+        React.createElement("h1", {}, "I am H1 Tag"),
+        React.createElement("h2", {}, "I am H2 Tag")] 
+    )
+]);
+root.render(parent); 
+
+
 /** 
  * Takes heading object which is React Elemet
  * Converts that React Element into HTML tag which can be understand by Browser
@@ -20,15 +36,3 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
  *    </div>
  * </div>
  */
-
-const parent =  React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child1"},[ 
-        React.createElement("h1", {}, "I am H1 Tag"),
-        React.createElement("h2", {}, "I am H2 Tag")] 
-    ),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "I am H1 Tag"),
-        React.createElement("h2", {}, "I am H2 Tag")] 
-    )
-]);
-root.render(parent); 
