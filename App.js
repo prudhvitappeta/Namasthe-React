86fe4fabc5d97c2c63130183cccfe1a8d035c9df
Prudhvi Raj Tappeta <prudhvitappeta@gmail.com>
2023-03-28 01:29:36 +0530

    Episode 3 - Part 1

--- App.js
+++ App.js
@@ -1,38 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading", xyz:"tagValue"}, "Hello World from React!");
-console.log(heading); // retunrs JS object which is React Element
+//JSX (Transpiled before it reaches to JS Engine) by Parcel through Babel.
+
+//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTML Element(rendered) 
+const jsxHeader = (
+    <h1 className="head" tabIndex="5" >
+        Namasthe React
+    </h1>
+);
+
+console.log(jsxHeader);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent =  React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child1"},[ 
-        React.createElement("h1", {}, "I am H1 Tag"),
-        React.createElement("h2", {}, "I am H2 Tag")] 
-    ),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "I am H1 Tag"),
-        React.createElement("h2", {}, "I am H2 Tag")] 
-    )
-]);
-root.render(parent); 
 
-
-/** 
- * Takes heading object which is React Elemet
- * Converts that React Element into HTML tag which can be understand by Browser
- * Reander this HTML tag inside the root element.(appendChild)
-*/
-//root.render(heading); 
-
-/**
- * <div id="parent">
-*    <div id="child1">
- *      <h1>I am h1 Tag </h1>
- *      <h2>I am h2 Tag </h2>
- *    </div>
- *    <div id="child2">
- *      <h1>I am h1 Tag </h1>
- *      <h2>I am h2 Tag </h2>
- *    </div>
- * </div>
- */
+root.render(jsxHeader);
