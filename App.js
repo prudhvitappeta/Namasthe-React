80fa988e0ac706934187ec711cc4186bdd5ca2a2
Prudhvi Raj Tappeta <prudhvitappeta@gmail.com>
2023-04-07 00:34:40 +0530

    Episode-04 Part 01 coding

--- App.js
+++ App.js
@@ -1,37 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX (Transpiled before it reaches to JS Engine) by Parcel through Babel.
-//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTML Element(rendered) 
+const Header = () => {
+    return(
+        <div className="header-container">
+            <div className="logo-container">
+                <img className="logo" src="https://static.vecteezy.com/system/resources/previews/006/395/395/original/food-point-logo-symbol-and-icon-template-to-show-the-location-of-the-food-seller-vector.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-const Title1 = () => (
-    <h1 className="title">
-        Namasthe React in Title1
-    </h1>
-);
-
-const Title2 = () => {
-    return ( 
-        <h1 className="title">
-            Namasthe React in Title2 using return keyword. It is same like Title1.
-        </h1>
+const RestaurantCard = () => {
+    return(
+        <div className="res-card">
+            <img className="res-logo" src="https://b.zmtcdn.com/data/pictures/chains/8/19937288/b8f98cd0239c1792920ffd087a83f9fa_o2_featured_v2.jpg?output-format=webp" />
+            <h3>Pista House</h3>
+            <h5>Biryani, Haleem, Paya, Chiken</h5>
+            <h5>3.8 star</h5>
+            <h5>20 minutes</h5>
+        </div>
     );
 }
 
-//Component composition == Component inside another Component
-const Headings = () => (
-// <React.Fragment> </React.Fragment> == <> </>
-    <React.Fragment> 
-        <Title1 />
-        <Title2 />
-        <h1 className="headings">
-            This is Heading from React functional component
-        </h1>
-    </React.Fragment>
-);
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">
+                search
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    );
+};
+
+const AppContainer = () => {
+    return(
+        <div className="app-container">
+            <div className="header">
+                <Header/>    
+            </div>
+            <div className="body">
+                <Body/>    
+            </div>    
+        </div>
+        
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Headings />);
-
-const root2 = ReactDOM.createRoot(document.getElementById('root2'));
-root2.render(<Headings />);
+root.render(<AppContainer />);
